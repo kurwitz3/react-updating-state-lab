5b394667a091493afa2192cbd88ad4d49406bf59
kurwitz3 <kurwitz3@aol.com>
2021-02-26 18:52:39 -0500

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,26 @@
-// Code DigitalClicker Component Here
+import React,{Component} from 'react'
+
+class DigitalClicker extends Component{
+    constructor(){
+        super()
+        this.state={
+           timesClicked: 0 
+        }
+    }
+    buttonClick = (e) => {
+        this.setState(previousState => {
+            return {
+              timesClicked: previousState.timesClicked + 1
+            }
+          })
+    }
+    render(){
+        return(
+            <div>
+               <button onClick={this.buttonClick} label={this.state.timesClicked}>{this.state.timesClicked}</button>
+            </div>
+        )
+    }
+}
+
+export default DigitalClicker
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,48 @@
-// Code YouTubeDebugger Component Here
+import React, {Component} from 'react'
+
+class YouTubeDebugger extends Component{
+    constructor(){
+        super()
+        this.state={
+            errors: [],
+            user: null,
+            settings: {
+              bitrate: 8,
+              video: {
+                resolution: '1080p'
+              }
+            } 
+        }
+    }
+
+    updateBirate = () =>{
+        this.setState({
+            settings:{
+                ...this.state.settings,
+                bitrate: 12
+            }
+        })
+    }
+
+    updateResolution = () => {
+        this.setState({
+            settings:{
+                bitrate: 8,
+                video:{
+                  resolution:  '720p'
+                }
+            }
+        })
+    }
+
+    render(){
+        return(
+            <div>
+                <button onClick={this.updateBirate} className='bitrate'></button>
+                <button onClick={this.updateResolution} className='resolution'></button>
+            </div>
+        )
+    }
+}
+
+export default YouTubeDebugger
